02136e72b0b0e17ca31da3c64df5ccc1f125ec83
signature2017 <joel.cabrera@envirostudies.org>
2017-02-02 23:17:54 +0000

    idk

--- mta.js
+++ mta.js
@@ -1,14 +1,12 @@
 /*global $*/
-$(document).ready(function() {
-    
-$.getJSON('https://ajar-target.gomix.me/stops', function(response){
-    
-    $("body").append(response[401].stop_name);
-    //console.log(Object.keys(response))
-       var trainChoice = "4";
-        var stopNumber = "02"
-    $("body").append(response[trainChoice=stopNumber].stop_name);
-    
-});
 
+$.getJSON('https://ajar-target.gomix.me/stops', function(response) {
+   console.log(response);
+   var keys = Object.keys(response);
+   for (i = 0; i < keys.length; i++) {
+       $("body").append (
+           response[keys[i]].stop_name 
+           );
+   }
 });
+    
